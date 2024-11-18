5d872312e57ae34b1c6cfaeddf932e566c27adc6
Programming-School-Pro-Coding <mohamedehabam99@hotmail.com>
2024-11-18 09:41:03 +0200

    Add Responsivity to timeline

--- app/_components/Timeline.js
+++ app/_components/Timeline.js
@@ -10,18 +10,19 @@ const Timeline = () => {
 
   return (
     <div className="bg-gray-50 py-12 relative flex flex-col gap-11 items-center justify-center">
-      <div className="text-center">
+      <div className="text-center px-4">
         <h2 className="text-2xl font-bold text-[#B7A580]">رحلتنا</h2>
         <p className="font-extrabold text-xl leading-[54px] text-[#1A1313]">
           رحلة تطور شركتنا عبر السنين
         </p>
       </div>
       <div className="relative w-full">
-        <div className="flex items-center justify-center gap-20 mx-8">
+        {/* Top Cards */}
+        <div className="flex items-center justify-center gap-6 lg:gap-20 flex-wrap sm:flex-nowrap px-4 sm:gap-4">
           {topCards.map(({ year, description }) => (
             <div
               key={year}
-              className="rounded-xl py-6 px-[14px] gap-3 bg-white flex flex-col items-center justify-center"
+              className="rounded-xl py-6 px-4 gap-3 bg-white flex flex-col items-center justify-center max-w-[200px] sm:max-w-full"
             >
               <h4 className="font-black text-base text-[#B7A580] leading-[9.04px]">
                 {year}
@@ -32,7 +33,8 @@ const Timeline = () => {
             </div>
           ))}
         </div>
-        <div className="relative my-8 flex items-center justify-center gap-44 w-full border-t border-[#DEDEE9]">
+        {/* Connector Dots */}
+        <div className="relative my-8 flex items-center justify-center sm:justify-evenly gap-8 lg:gap-10 sm:gap-2 w-full border-t border-[#DEDEE9]">
           {Array.from({ length: 7 }).map((_, index) => (
             <div
               key={index}
@@ -40,11 +42,12 @@ const Timeline = () => {
             />
           ))}
         </div>
-        <div className="flex items-center justify-center gap-20 mx-8">
+        {/* Bottom Cards */}
+        <div className="flex items-center justify-center gap-6 lg:gap-20 flex-wrap sm:flex-nowrap px-4 sm:gap-4">
           {bottomCards.map(({ year, description }) => (
             <div
               key={year}
-              className="rounded-xl py-6 px-[14px] gap-3 bg-white flex flex-col items-center justify-center"
+              className="rounded-xl py-6 px-4 gap-3 bg-white flex flex-col items-center justify-center max-w-[200px] sm:max-w-full"
             >
               <h4 className="font-black text-base text-[#B7A580] leading-[9.04px]">
                 {year}
